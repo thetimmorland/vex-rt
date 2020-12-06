b62646c202ba5f0e494d901ea811f743ca29b22a
Timothy Morland <me@timothy-morland.com>
2020-12-06 01:30:49 -0500

    fixup! Monster commit: working bindgen

--- examples/motors.rs
+++ examples/motors.rs
@@ -4,7 +4,7 @@
 extern crate vex_rt as rt;
 
 #[no_mangle]
-extern "C" fn initialize() {
+extern "C" fn opcontrol() {
     let peripherals = vex_rt::Peripherals::take().unwrap();
     peripherals.port1.as_motor().set_voltage(32);
 }
