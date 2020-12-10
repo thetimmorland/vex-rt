b765c282f18b2d86af53f99cb52f41eefb82ba67
Timothy Morland <me@timothy-morland.com>
2020-12-10 16:52:29 -0500

    better entry macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,32 @@
 #![no_std]
 
-pub use vex_rt_core::*;
+use core::panic::PanicInfo;
+use libc_alloc::LibcAlloc;
+use libc_print::libc_println;
+
+mod bindings;
+mod motor;
+mod peripherals;
+mod smart_port;
+
+pub use motor::*;
+pub use peripherals::*;
+pub use smart_port::*;
+
 pub use vex_rt_macros::*;
+
+pub use spin::*;
+
+#[panic_handler]
+fn panic(panic_info: &PanicInfo) -> ! {
+    if let Some(s) = panic_info.payload().downcast_ref::<&str>() {
+        libc_println!("panic occurred!: {:?}", s);
+    } else {
+        libc_println!("panic occurred!");
+    }
+
+    loop {}
+}
+
+#[global_allocator]
+static ALLOCATOR: LibcAlloc = LibcAlloc;
