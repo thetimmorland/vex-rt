1e60c7afe1047455698940d419231224ad85b9ff
Nick Mertin <nickmertin@gmail.com>
2020-12-10 18:53:26 -0500

    Implement spawning new tasks

--- core/src/rtos.rs
+++ core/src/rtos.rs
@@ -1,11 +1,10 @@
-use alloc::format;
-use core::convert::TryInto;
-use core::option::Option;
+use alloc::boxed::Box;
 use core::ptr::null_mut;
 use core::time::Duration;
-use rcstring::*;
 
 use crate::bindings;
+use crate::error::*;
+use crate::util::*;
 
 pub fn time_since_start() -> Duration {
     unsafe { Duration::from_millis(bindings::millis().into()) }
@@ -16,22 +15,69 @@ pub struct Task(bindings::task_t);
 impl Task {
     pub fn delay(dur: Duration) {
         unsafe {
-            bindings::task_delay(dur.as_millis().try_into().unwrap());
+            bindings::task_delay(dur.as_millis() as u32);
         }
     }
 
-    pub fn find_by_name(name: &str) -> Option<Task> {
-        match CString::new(&format!("{}\0", name)) {
-            Ok(cname) => {
-                let ptr = unsafe { bindings::task_get_by_name(cname.into_raw()) };
-                if ptr == null_mut() {
-                    None
-                } else {
-                    Some(Task(ptr))
-                }
+    pub fn find_by_name(name: &str) -> Result<Task, Error> {
+        as_cstring(name, |cname| {
+            let ptr = unsafe { bindings::task_get_by_name(cname.into_raw()) };
+            if ptr == null_mut() {
+                todo!("Error")
+            } else {
+                Ok(Task(ptr))
             }
-            Err(_) => None,
+        })
+    }
+
+    pub fn spawn<F: FnOnce() + Send>(f: F) -> Result<Task, Error> {
+        Task::spawn_ext(
+            "",
+            bindings::TASK_PRIORITY_DEFAULT,
+            bindings::TASK_STACK_DEPTH_DEFAULT as u16,
+            f,
+        )
+    }
+
+    pub fn spawn_ext<F: FnOnce() + Send>(
+        name: &str,
+        priority: u32,
+        stack_depth: u16,
+        f: F,
+    ) -> Result<Task, Error> {
+        extern "C" fn run<F: FnOnce()>(arg: *mut libc::c_void) {
+            let cb_box: Box<F> = unsafe { Box::from_raw(arg as *mut F) };
+            cb_box()
         }
+
+        let cb = Box::new(f);
+        unsafe {
+            let arg = Box::into_raw(cb);
+            let r = Task::spawn_raw(name, priority, stack_depth, run::<F>, arg as *mut _);
+            if let Err(_) = r {
+                // We need to re-box the pointer if the task could not be created, to avoid a
+                // memory leak.
+                Box::from_raw(arg);
+            }
+            r
+        }
+    }
+
+    pub unsafe fn spawn_raw(
+        name: &str,
+        priority: u32,
+        stack_depth: u16,
+        f: unsafe extern "C" fn(arg1: *mut libc::c_void),
+        arg: *mut libc::c_void,
+    ) -> Result<Task, Error> {
+        as_cstring(name, |cname| {
+            let task = bindings::task_create(Some(f), arg, priority, stack_depth, cname.into_raw());
+            if task != null_mut() {
+                Ok(Task(task))
+            } else {
+                Err(from_errno())
+            }
+        })
     }
 }
 
@@ -45,7 +91,7 @@ impl Loop {
         unsafe {
             Loop {
                 last_time: bindings::millis(),
-                delta: delta.as_millis().try_into().unwrap(),
+                delta: delta.as_millis() as u32,
             }
         }
     }
