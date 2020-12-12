5576b2c4039c2a5ae7be23691fd3e6ff9b1f0d44
Nick Mertin <nickmertin@gmail.com>
2020-12-11 21:10:25 -0500

    Update task example to use task names

--- examples/task.rs
+++ examples/task.rs
@@ -14,10 +14,13 @@ impl Robot {
     fn initialize() -> Self {
         let mut x = 0;
         let mut l = Loop::new(Duration::from_secs(1));
-        Task::spawn(move || loop {
-            println!("{}", x);
-            x += 1;
-            l.delay()
+        Task::spawn_ext("test", 8, 0x2000, move || {
+            println!("Task name: {}", Task::current().name());
+            loop {
+                println!("{}", x);
+                x += 1;
+                l.delay()
+            }
         });
         Robot
     }
