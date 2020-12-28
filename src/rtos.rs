40c6ff8421c18f1fb0131ad46da66b18f5e347e7
Nick Mertin <nickmertin@gmail.com>
2020-12-28 14:01:10 -0500

    Implement Mutex

--- src/rtos.rs
+++ src/rtos.rs
@@ -1,12 +1,16 @@
 use alloc::boxed::Box;
 use alloc::format;
 use alloc::string::String;
-use core::ptr::null_mut;
-use core::time::Duration;
+use core::fmt::{Debug, Display, Formatter};
+use core::{cell::UnsafeCell, fmt};
+use core::{ops::Deref, time::Duration};
+use core::{ops::DerefMut, ptr::null_mut};
 
 use crate::bindings;
 use crate::error::*;
 use crate::util::*;
+
+const TIMEOUT_MAX: u32 = 0xffffffff;
 
 pub fn time_since_start() -> Duration {
     unsafe { Duration::from_millis(bindings::millis().into()) }
@@ -90,6 +94,125 @@ impl Task {
     }
 }
 
+pub struct Mutex<T: ?Sized> {
+    mutex: bindings::mutex_t,
+    data: UnsafeCell<T>,
+}
+
+impl<T> Mutex<T> {
+    pub fn new(data: T) -> Self {
+        Self::try_new(data).unwrap_or_else(|err| panic!("failed to create mutex: {:?}", err))
+    }
+
+    pub fn try_new(data: T) -> Result<Self, Error> {
+        let mutex = unsafe { bindings::mutex_create() };
+        if mutex != null_mut() {
+            Ok(Self {
+                data: UnsafeCell::new(data),
+                mutex,
+            })
+        } else {
+            Err(from_errno())
+        }
+    }
+}
+
+impl<T: ?Sized> Mutex<T> {
+    pub fn lock<'a>(&'a self) -> Result<MutexGuard<'a, T>, Error> {
+        if unsafe { bindings::mutex_take(self.mutex, TIMEOUT_MAX) } {
+            Ok(MutexGuard(self))
+        } else {
+            Err(from_errno())
+        }
+    }
+
+    pub fn poll<'a>(&'a self) -> Option<MutexGuard<'a, T>> {
+        if unsafe { bindings::mutex_take(self.mutex, 0) } {
+            Some(MutexGuard(self))
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: ?Sized> Drop for Mutex<T> {
+    fn drop(&mut self) {
+        unsafe { bindings::mutex_delete(self.mutex) }
+    }
+}
+
+impl<T: ?Sized + Debug> Debug for Mutex<T> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        match self.poll() {
+            Some(guard) => f.debug_struct("Mutex").field("data", &&*guard).finish(),
+            None => {
+                struct LockedPlaceholder;
+                impl Debug for LockedPlaceholder {
+                    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+                        f.write_str("<locked>")
+                    }
+                }
+
+                f.debug_struct("Mutex")
+                    .field("data", &LockedPlaceholder)
+                    .finish()
+            }
+        }
+    }
+}
+
+impl<T: ?Sized + Default> Default for Mutex<T> {
+    fn default() -> Self {
+        Self::new(Default::default())
+    }
+}
+
+impl<T> From<T> for Mutex<T> {
+    fn from(data: T) -> Self {
+        Self::new(data)
+    }
+}
+
+pub struct MutexGuard<'a, T: ?Sized>(&'a Mutex<T>);
+
+impl<T: ?Sized> Deref for MutexGuard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.0.data.get() }
+    }
+}
+
+impl<T: ?Sized> DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.0.data.get() }
+    }
+}
+
+impl<T: ?Sized> Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        if !unsafe { bindings::mutex_give(self.0.mutex) } {
+            panic!("failed to return mutex: {:?}", from_errno());
+        }
+    }
+}
+
+impl<T: ?Sized + Debug> Debug for MutexGuard<'_, T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        Debug::fmt(&**self, f)
+    }
+}
+
+impl<T: ?Sized + Display> Display for MutexGuard<'_, T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        (**self).fmt(f)
+    }
+}
+
+impl<T: ?Sized> !Send for MutexGuard<'_, T> {}
+
+unsafe impl<T: ?Sized + Sync> Sync for MutexGuard<'_, T> {}
+
 pub struct Loop {
     last_time: u32,
     delta: u32,
@@ -141,7 +264,7 @@ impl GenericSleep {
         match self {
             GenericSleep::NotifyTake(timeout) => {
                 let timeout =
-                    timeout.map_or(0xffffffff, |v| (time_since_start() - v).as_millis() as u32);
+                    timeout.map_or(TIMEOUT_MAX, |v| (time_since_start() - v).as_millis() as u32);
                 unsafe { bindings::task_notify_take(true, timeout) }
             }
             GenericSleep::Timestamp(v) => {
