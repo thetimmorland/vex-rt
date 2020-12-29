2e325e2d66f0b3d2d68c80f20ce00dcef1179299
Nick Mertin <nickmertin@gmail.com>
2020-12-29 12:07:00 -0500

    Update select example

--- examples/select.rs
+++ examples/select.rs
@@ -12,20 +12,6 @@ struct Robot;
 #[entry]
 impl Robot {
     fn initialize() -> Self {
-        let mut x = 0;
-        let mut l = Loop::new(Duration::from_secs(1));
-        let ctx = Context::new_global();
-        Task::spawn(move || {
-            println!("Task name: {}", Task::current().name());
-            loop {
-                println!("{}", x);
-                x += 1;
-                select! {
-                    _ = l.next() => (),
-                    _ = ctx.done() => (),
-                }
-            }
-        });
         Robot
     }
     fn autonomous(&self) {
@@ -33,6 +19,22 @@ impl Robot {
     }
     fn opcontrol(&self) {
         println!("opcontrol");
+        let ctx = Context::new_global();
+        Task::spawn({
+            let mut x = 0;
+            let mut l = Loop::new(Duration::from_secs(1));
+            let ctx = ctx.clone();
+            move || loop {
+                println!("{}", x);
+                x += 1;
+                select! {
+                    _ = l.next() => {},
+                    _ = ctx.done() => break,
+                }
+            }
+        });
+        Task::delay(Duration::from_secs(10));
+        ctx.cancel();
     }
     fn disable(&self) {
         println!("disabled");
