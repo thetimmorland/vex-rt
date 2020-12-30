f4880d9adb58910a1774877ea764cedfbd7d70bc
Nick Mertin <nickmertin@gmail.com>
2020-12-30 16:23:58 -0500

    Clean up select macro definition

--- src/macros/select.rs
+++ src/macros/select.rs
@@ -1,51 +1,4 @@
 #[macro_export]
-#[doc(hidden)]
-macro_rules! select_head {
-    ($event:expr,) => {$event};
-    ($event:expr, $($rest:expr,)+) => {($event, $crate::select_head!($($rest,)*))}
-}
-
-#[macro_export]
-#[doc(hidden)]
-macro_rules! select_match {
-    { $event:expr; $cons:expr; $_:expr, } => {
-        match $crate::rtos::Selectable::poll($event) {
-            ::core::result::Result::Ok(r) => break $cons(r),
-            ::core::result::Result::Err(s) => s,
-        }
-    };
-    { $events:expr; $cons:expr; $_:expr, $($rest:expr,)+ } => {
-        match $crate::rtos::Selectable::poll($events.0) {
-            ::core::result::Result::Ok(r) => break $cons(::core::result::Result::Ok(r)),
-            ::core::result::Result::Err(s) => (s, $crate::select_match!{$events.1; |r| $cons(::core::result::Result::Err(r)); $($rest,)*}),
-        }
-    };
-}
-
-#[macro_export]
-#[doc(hidden)]
-macro_rules! select_body {
-    { $result:expr; $var:pat => $body:expr, } => {
-        match $result {
-            $var => $body,
-        }
-    };
-    { $result:expr; $var:pat => $body:expr, $($vars:pat => $bodys:expr,)+ } => {
-        match $result {
-            ::core::result::Result::Ok($var) => $body,
-            ::core::result::Result::Err(r) => $crate::select_body!{r; $($vars => $bodys,)*},
-        }
-    };
-}
-
-#[macro_export]
-#[doc(hidden)]
-macro_rules! select_sleep {
-    ($events:expr; $_:expr,) => {$events.sleep()};
-    ($events:expr; $_:expr, $($rest:expr,)+) => {$events.0.sleep().combine($crate::select_sleep!($events.1; $($rest,)+))};
-}
-
-#[macro_export]
 /// Selects over a range of possible future events, processing exactly one.
 /// Inspired by equivalent behaviours in other programming languages such as Go
 /// and Kotlin, and ultimately the `select` system call from POSIX.
@@ -77,7 +30,59 @@ macro_rules! select {
         let mut events = $crate::select_head!($($event,)+);
         $crate::select_body!{loop {
             events = $crate::select_match!{events; |r| r; $($event,)+};
-            $crate::select_sleep!(events; $($event,)+).sleep();
+            $crate::rtos::GenericSleep::sleep($crate::select_sleep!(events; $($event,)+));
         }; $($var => $body,)+}
     }};
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! select_head {
+    ($event:expr,) => {$event};
+    ($event:expr, $($rest:expr,)+) => {($event, $crate::select_head!($($rest,)*))}
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! select_match {
+    { $event:expr; $cons:expr; $_:expr, } => {
+        match $crate::rtos::Selectable::poll($event) {
+            ::core::result::Result::Ok(r) => break $cons(r),
+            ::core::result::Result::Err(s) => s,
+        }
+    };
+    { $events:expr; $cons:expr; $_:expr, $($rest:expr,)+ } => {
+        match $crate::rtos::Selectable::poll($events.0) {
+            ::core::result::Result::Ok(r) => break $cons(::core::result::Result::Ok(r)),
+            ::core::result::Result::Err(s) => (
+                s,
+                $crate::select_match!{$events.1; |r| $cons(::core::result::Result::Err(r)); $($rest,)*}
+            ),
+        }
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! select_body {
+    { $result:expr; $var:pat => $body:expr, } => {
+        match $result {
+            $var => $body,
+        }
+    };
+    { $result:expr; $var:pat => $body:expr, $($vars:pat => $bodys:expr,)+ } => {
+        match $result {
+            ::core::result::Result::Ok($var) => $body,
+            ::core::result::Result::Err(r) => $crate::select_body!{r; $($vars => $bodys,)*},
+        }
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! select_sleep {
+    ($event:expr; $_:expr,) => {$crate::rtos::Selectable::sleep(&$event)};
+    ($events:expr; $_:expr, $($rest:expr,)+) => {
+        $crate::rtos::Selectable::sleep(&$events.0).combine($crate::select_sleep!($events.1; $($rest,)+))
+    };
 }
